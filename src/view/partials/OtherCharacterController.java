b2e9520cc7b65c2860d817b76ebefb9e5bdda193
Josh <the-cheese@hotmail.com>
2014-09-16 16:31:09 +1200

    I AM WORKING ON THE BUTTONS, TO BE CONTINUED

--- src/view/partials/OtherCharacterController.java
+++ src/view/partials/OtherCharacterController.java
@@ -1,7 +1,14 @@
 package view.partials;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import jefXif.WindowController;
 
 /**
@@ -12,7 +19,38 @@ public class OtherCharacterController extends WindowController {
 
 	@FXML
 	private void handleNewCharacterAction(ActionEvent event) {
-		
+		showNewChar();
+	}
+	
+	@FXML
+	private void showNewChar()
+	{
+		try
+		{
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(this.getClass().getResource("NewCharacterPartial.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+	
+	        // Create the dialog Stage.
+	        Stage dialogStage = new Stage();
+	        dialogStage.setTitle("Create New Character");
+	        dialogStage.initModality(Modality.WINDOW_MODAL);
+	        dialogStage.initOwner(this.getInterface().getPrimaryStage());
+	        Scene scene = new Scene(page);
+	        dialogStage.setScene(scene);
+	
+	        // Set the person into the controller.
+	        NewCharacterController controller = loader.getController();
+	        controller.setDialogStage(dialogStage);
+	        //controller.setArmor((MagicArmor)item);
+	
+	        // Show the dialog and wait until the user closes it
+	        dialogStage.showAndWait();
+		}
+		catch(IOException e)
+		{
+	        e.printStackTrace();
+		}
 	}
 	
 	@Override
