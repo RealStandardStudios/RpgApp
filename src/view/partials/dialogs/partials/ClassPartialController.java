91a4f13504db084ea9584d37db38df95a2c8834d
Matthew <lololirpro@gmail.com>
2014-09-22 23:44:58 -0700

    fixed that tab thing

--- src/view/partials/dialogs/partials/ClassPartialController.java
+++ src/view/partials/dialogs/partials/ClassPartialController.java
@@ -5,7 +5,6 @@ import java.io.File;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
-import javafx.collections.ListChangeListener.Change;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Accordion;
@@ -65,7 +64,7 @@ public class ClassPartialController extends NewCharacterPartialController {
 	// region Class Progression table
 	@FXML
 	private Tab tabLevelTable;
-	
+
 	@FXML
 	private TableView<LevelTableRow> tableLevelTable;
 
@@ -229,229 +228,231 @@ public class ClassPartialController extends NewCharacterPartialController {
 	public void initialize() {
 		classes = FXCollections.observableArrayList();
 		readClasses();
-		
+
 		cboClasses.setItems(classes);
-		//region Init the Class Progression Level Table with columns
-				columnLevel.setCellValueFactory(cellData -> cellData.getValue()
-						.getLevelNumProperty());
-				columnBAB.setCellValueFactory(cellData -> cellData.getValue()
-						.getBABProperty());
-				columnFort.setCellValueFactory(cellData -> cellData.getValue()
-						.getFortSave().getBaseValueProperty());
-				columnRef.setCellValueFactory(cellData -> cellData.getValue()
-						.getRefSave().getBaseValueProperty());
-				columnWill.setCellValueFactory(cellData -> cellData.getValue()
-						.getWillSave().getBaseValueProperty());
-				columnSpecial.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpecialProperty());
-				//endregion
+		// region Init the Class Progression Level Table with columns
+		columnLevel.setCellValueFactory(cellData -> cellData.getValue()
+				.getLevelNumProperty());
+		columnBAB.setCellValueFactory(cellData -> cellData.getValue()
+				.getBABProperty());
+		columnFort.setCellValueFactory(cellData -> cellData.getValue()
+				.getFortSave().getBaseValueProperty());
+		columnRef.setCellValueFactory(cellData -> cellData.getValue()
+				.getRefSave().getBaseValueProperty());
+		columnWill.setCellValueFactory(cellData -> cellData.getValue()
+				.getWillSave().getBaseValueProperty());
+		columnSpecial.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpecialProperty());
+		// endregion
 
-				//region Init the Class Progression Spell Level Table with columns
-				columnLevelSpells.setCellValueFactory(cellData -> cellData.getValue()
-						.getLevelNumProperty());
-				column0.setCellValueFactory(cellData -> cellData.getValue().getSPD()[0]);
-				column1st
-						.setCellValueFactory(cellData -> cellData.getValue().getSPD()[1]);
-				column2nd
-						.setCellValueFactory(cellData -> cellData.getValue().getSPD()[2]);
-				column3rd
-						.setCellValueFactory(cellData -> cellData.getValue().getSPD()[3]);
-				column4th
-						.setCellValueFactory(cellData -> cellData.getValue().getSPD()[4]);
-				column5th
-						.setCellValueFactory(cellData -> cellData.getValue().getSPD()[5]);
-				column6th
-						.setCellValueFactory(cellData -> cellData.getValue().getSPD()[6]);
-				column7th
-						.setCellValueFactory(cellData -> cellData.getValue().getSPD()[7]);
-				column8th
-						.setCellValueFactory(cellData -> cellData.getValue().getSPD()[8]);
-				column9th
-						.setCellValueFactory(cellData -> cellData.getValue().getSPD()[9]);
-				//endregion
+		// region Init the Class Progression Spell Level Table with columns
+		columnLevelSpells.setCellValueFactory(cellData -> cellData.getValue()
+				.getLevelNumProperty());
+		column0.setCellValueFactory(cellData -> cellData.getValue().getSPD()[0]);
+		column1st
+				.setCellValueFactory(cellData -> cellData.getValue().getSPD()[1]);
+		column2nd
+				.setCellValueFactory(cellData -> cellData.getValue().getSPD()[2]);
+		column3rd
+				.setCellValueFactory(cellData -> cellData.getValue().getSPD()[3]);
+		column4th
+				.setCellValueFactory(cellData -> cellData.getValue().getSPD()[4]);
+		column5th
+				.setCellValueFactory(cellData -> cellData.getValue().getSPD()[5]);
+		column6th
+				.setCellValueFactory(cellData -> cellData.getValue().getSPD()[6]);
+		column7th
+				.setCellValueFactory(cellData -> cellData.getValue().getSPD()[7]);
+		column8th
+				.setCellValueFactory(cellData -> cellData.getValue().getSPD()[8]);
+		column9th
+				.setCellValueFactory(cellData -> cellData.getValue().getSPD()[9]);
+		// endregion
 
-				//region Init the Class Progression Spells Known Table with columns
-				columnLevelSpellsKnown.setCellValueFactory(cellData -> cellData
-						.getValue().getLevelNumProperty());
-				column0Known.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[0]);
-				column1stKnown.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[1]);
-				column2ndKnown.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[2]);
-				column3rdKnown.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[3]);
-				column4thKnown.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[4]);
-				column5thKnown.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[5]);
-				column6thKnown.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[6]);
-				column7thKnown.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[7]);
-				column8thKnown.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[8]);
-				column9thKnown.setCellValueFactory(cellData -> cellData.getValue()
-						.getSpellsKnown()[9]);
-				//endregion
+		// region Init the Class Progression Spells Known Table with columns
+		columnLevelSpellsKnown.setCellValueFactory(cellData -> cellData
+				.getValue().getLevelNumProperty());
+		column0Known.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[0]);
+		column1stKnown.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[1]);
+		column2ndKnown.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[2]);
+		column3rdKnown.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[3]);
+		column4thKnown.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[4]);
+		column5thKnown.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[5]);
+		column6thKnown.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[6]);
+		column7thKnown.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[7]);
+		column8thKnown.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[8]);
+		column9thKnown.setCellValueFactory(cellData -> cellData.getValue()
+				.getSpellsKnown()[9]);
+		// endregion
 
-				//region Init the Monk progression table with columns
-				columnFOB.setCellValueFactory(celldata -> celldata.getValue()
-						.getFlurryOfBlowsString());
-				columnUnarmed.setCellValueFactory(cellData -> cellData.getValue()
-						.getUnarmedDamageProperty());
-				columnAcBonus.setCellValueFactory(cellData -> cellData.getValue()
-						.getAcBonusProperty());
-				columnFastMovement.setCellValueFactory(cellData -> cellData.getValue()
-						.getFastMovementProperty());
-				//endregion
-				
-//				//region Init the features table with columns
-//				columnFeatureName.setCellValueFactory(cellData -> cellData.getValue()
-//						.getNameProperty());
-//				columnFeatureType.setCellValueFactory(cellData -> cellData.getValue()
-//						.getTypeProperty());
-//				columnFeatureDesctiption.setCellValueFactory(cellData -> cellData
-//						.getValue().getDescriptionProperty());
-//				// columnFeatureEffect.setCellValueFactory(cellData ->
-//				// cellData.getValue().getEffectProperty().get().getNameProperty());
-//				//endregion
+		// region Init the Monk progression table with columns
+		columnFOB.setCellValueFactory(celldata -> celldata.getValue()
+				.getFlurryOfBlowsString());
+		columnUnarmed.setCellValueFactory(cellData -> cellData.getValue()
+				.getUnarmedDamageProperty());
+		columnAcBonus.setCellValueFactory(cellData -> cellData.getValue()
+				.getAcBonusProperty());
+		columnFastMovement.setCellValueFactory(cellData -> cellData.getValue()
+				.getFastMovementProperty());
+		// endregion
 
-				// Array of Table Columns
-				levelTable = new TableColumn[] { columnLevel, columnBAB,
-						columnFort, columnRef, columnWill, columnSpecial };
-				// table.getcolumns().adListener(new
-				// ListChangeListenet<TableColumn<DataType,?>>() { }
-				tableLevelTable.getColumns().addListener(
-						new ListChangeListener<TableColumn<LevelTableRow, ?>>() {
-							public boolean suspended;
+		// //region Init the features table with columns
+		// columnFeatureName.setCellValueFactory(cellData -> cellData.getValue()
+		// .getNameProperty());
+		// columnFeatureType.setCellValueFactory(cellData -> cellData.getValue()
+		// .getTypeProperty());
+		// columnFeatureDesctiption.setCellValueFactory(cellData -> cellData
+		// .getValue().getDescriptionProperty());
+		// // columnFeatureEffect.setCellValueFactory(cellData ->
+		// // cellData.getValue().getEffectProperty().get().getNameProperty());
+		// //endregion
 
-							@Override
-							public void onChanged(
-									Change<? extends TableColumn<LevelTableRow, ?>> change) {
-								change.next();
+		// Array of Table Columns
+		levelTable = new TableColumn[] { columnLevel, columnBAB, columnFort,
+				columnRef, columnWill, columnSpecial };
+		// table.getcolumns().adListener(new
+		// ListChangeListenet<TableColumn<DataType,?>>() { }
+		tableLevelTable.getColumns().addListener(
+				new ListChangeListener<TableColumn<LevelTableRow, ?>>() {
+					public boolean suspended;
 
-								if (change.wasReplaced() && !suspended) {
-									this.suspended = true;
-									// array of table columns as defined above
-									tableLevelTable.getColumns().setAll(
-											levelTable);
-									this.suspended = false;
-								}
+					@Override
+					public void onChanged(
+							Change<? extends TableColumn<LevelTableRow, ?>> change) {
+						change.next();
 
-							}
+						if (change.wasReplaced() && !suspended) {
+							this.suspended = true;
+							// array of table columns as defined above
+							tableLevelTable.getColumns().setAll(levelTable);
+							this.suspended = false;
+						}
 
-						});
+					}
 
-				spellLevelTable = new TableColumn[] { columnLevelSpells, column0,
-						column1st, column2nd, column3rd, column4th, column5th,
-						column6th, column7th, column8th, column9th };
+				});
 
-				tableSpellLevelTable.getColumns().addListener(
-						new ListChangeListener<TableColumn<SpellLevelTableRow, ?>>() {
-							public boolean suspended;
+		spellLevelTable = new TableColumn[] { columnLevelSpells, column0,
+				column1st, column2nd, column3rd, column4th, column5th,
+				column6th, column7th, column8th, column9th };
 
-							@Override
-							public void onChanged(
-									Change<? extends TableColumn<SpellLevelTableRow, ?>> change) {
-								change.next();
+		tableSpellLevelTable.getColumns().addListener(
+				new ListChangeListener<TableColumn<SpellLevelTableRow, ?>>() {
+					public boolean suspended;
 
-								if (change.wasReplaced() && !suspended) {
-									this.suspended = true;
-									// array of table columns as defined above
-									tableSpellLevelTable.getColumns().setAll(
-											spellLevelTable);
-									this.suspended = false;
-								}
+					@Override
+					public void onChanged(
+							Change<? extends TableColumn<SpellLevelTableRow, ?>> change) {
+						change.next();
 
-							}
+						if (change.wasReplaced() && !suspended) {
+							this.suspended = true;
+							// array of table columns as defined above
+							tableSpellLevelTable.getColumns().setAll(
+									spellLevelTable);
+							this.suspended = false;
+						}
 
-						});
+					}
 
-				spellKnowenTable = new TableColumn[] { columnLevelSpellsKnown,
-						column0Known, column1stKnown, column2ndKnown, column3rdKnown,
-						column4thKnown, column5thKnown, column6thKnown, column7thKnown,
-						column8thKnown, column9thKnown };
-				
-				tableSpellsKnown.getColumns().addListener(
-						new ListChangeListener<TableColumn<SpellLevelTableRow, ?>>() {
-							public boolean suspended;
+				});
 
-							@Override
-							public void onChanged(
-									Change<? extends TableColumn<SpellLevelTableRow, ?>> change) {
-								change.next();
+		spellKnowenTable = new TableColumn[] { columnLevelSpellsKnown,
+				column0Known, column1stKnown, column2ndKnown, column3rdKnown,
+				column4thKnown, column5thKnown, column6thKnown, column7thKnown,
+				column8thKnown, column9thKnown };
 
-								if (change.wasReplaced() && !suspended) {
-									this.suspended = true;
-									// array of table columns as defined above
-									tableSpellsKnown.getColumns().setAll(
-											spellKnowenTable);
-									this.suspended = false;
-								}
+		tableSpellsKnown.getColumns().addListener(
+				new ListChangeListener<TableColumn<SpellLevelTableRow, ?>>() {
+					public boolean suspended;
 
-							}
+					@Override
+					public void onChanged(
+							Change<? extends TableColumn<SpellLevelTableRow, ?>> change) {
+						change.next();
 
-						});
+						if (change.wasReplaced() && !suspended) {
+							this.suspended = true;
+							// array of table columns as defined above
+							tableSpellsKnown.getColumns().setAll(
+									spellKnowenTable);
+							this.suspended = false;
+						}
 
-				monkTable = new TableColumn[] {
-					columnFOB,columnUnarmed,columnAcBonus,columnFastMovement	
-				};
-				
-				tableMonkTable.getColumns().addListener(
-						new ListChangeListener<TableColumn<MonkLevelTableRow, ?>>(){
-							public boolean suspended;
-							
-							@Override
-							public void onChanged(Change<? extends TableColumn<MonkLevelTableRow,?>> change) {
-								change.next();
-								
-								if(change.wasReplaced() && !suspended) {
-									this.suspended = true;
-									
-									tableMonkTable.getColumns().setAll(monkTable);
-								}
-							}
-						});
+					}
 
-//				featuresTable = new TableColumn[] { columnFeatureName,
-//						columnFeatureType, columnFeatureDesctiption,
-//						columnFeatureEffect };
-//
-//				tableFeatures.getColumns().addListener(
-//						new ListChangeListener<TableColumn<Feature, ?>>() {
-//							public boolean suspended;
-//
-//							@Override
-//							public void onChanged(
-//									Change<? extends TableColumn<Feature, ?>> change) {
-//								change.next();
-//
-//								if (change.wasReplaced() && !suspended) {
-//									this.suspended = true;
-//									tableFeatures.getColumns().setAll(featuresTable);
-//									this.suspended = false;
-//								}
-//							}
-//						});
-			}
+				});
+
+		monkTable = new TableColumn[] { columnFOB, columnUnarmed,
+				columnAcBonus, columnFastMovement };
+
+		tableMonkTable.getColumns().addListener(
+				new ListChangeListener<TableColumn<MonkLevelTableRow, ?>>() {
+					public boolean suspended;
+
+					@Override
+					public void onChanged(
+							Change<? extends TableColumn<MonkLevelTableRow, ?>> change) {
+						change.next();
+
+						if (change.wasReplaced() && !suspended) {
+							this.suspended = true;
+
+							tableMonkTable.getColumns().setAll(monkTable);
+						}
+					}
+				});
+
+		// featuresTable = new TableColumn[] { columnFeatureName,
+		// columnFeatureType, columnFeatureDesctiption,
+		// columnFeatureEffect };
+		//
+		// tableFeatures.getColumns().addListener(
+		// new ListChangeListener<TableColumn<Feature, ?>>() {
+		// public boolean suspended;
+		//
+		// @Override
+		// public void onChanged(
+		// Change<? extends TableColumn<Feature, ?>> change) {
+		// change.next();
+		//
+		// if (change.wasReplaced() && !suspended) {
+		// this.suspended = true;
+		// tableFeatures.getColumns().setAll(featuresTable);
+		// this.suspended = false;
+		// }
+		// }
+		// });
+	}
 
 	private void readClasses() {
-		File file = new File(this.getClass().getResource("").getPath()+programRoot+"PathfinderData/data");
-		File featsFile = new File(file.getPath()+"\\Classes.cldf");
+		File file = new File(this.getClass().getResource("").getPath()
+				+ programRoot + "PathfinderData/data");
+		File featsFile = new File(file.getPath() + "\\Classes.cldf");
 		try {
 			classes.setAll(readDataFile(featsFile, Class.class));
 		} catch (Exception e) {
-			Dialogs.create().masthead("Read Error").masthead("Couldn't read the Class File Properly").message(e.getMessage()).showWarning();
+			Dialogs.create().masthead("Read Error")
+					.masthead("Couldn't read the Class File Properly")
+					.message(e.getMessage()).showWarning();
 			e.printStackTrace();
 		}
 	}
 
 	@Override
 	public void setData() {
-		
+
 	}
-	
+
 	@FXML
 	private void handleChangedClass(ActionEvent event) {
 		Class selectedClass = cboClasses.getSelectionModel().getSelectedItem();
@@ -465,17 +466,17 @@ public class ClassPartialController extends NewCharacterPartialController {
 		lblSkillRanksPerLevel.setText(selectedClass.getSkillRanksToString());
 		lblStartingWealthD6
 				.setText("D6x" + selectedClass.getStartingWealthD6());
-		lblWeaponProf.setText(selectedClass.getWeaponProfsToString());		
-		
+		lblWeaponProf.setText(selectedClass.getWeaponProfsToString());
+
 		tableLevelTable.setItems(selectedClass.getLeveltableRow());
-		
+
 		String tableRowType = selectedClass.getLeveltableRow().get(0)
 				.getClass().getName();
 		if (tableRowType != "pathfinder.data.Classes.Objects.LevelTable.SpellLevelTableRow")
 			tpProgression.getTabs().removeAll(tabSpellLevelTable,
 					tabSpellsKnown);
 		else {
-			tpProgression.getTabs().addAll(tabSpellLevelTable, tabSpellsKnown);
+			tpProgression.getTabs().add(tabSpellLevelTable);
 			SpellLevelTableRow[] levelTableRow = new SpellLevelTableRow[selectedClass
 					.getLeveltableRow().size()];
 			for (int i = 0; i < selectedClass.getLeveltableRow().size(); i++) {
@@ -485,7 +486,12 @@ public class ClassPartialController extends NewCharacterPartialController {
 			ObservableList<SpellLevelTableRow> spellLevelTables = FXCollections
 					.observableArrayList(levelTableRow);
 			tableSpellLevelTable.setItems(spellLevelTables);
-			tableSpellsKnown.setItems(spellLevelTables);
+			if(spellLevelTables.get(0).getSpellsKnown()[0]!=null) {
+				tpProgression.getTabs().add(tabSpellsKnown);
+				tableSpellsKnown.setItems(spellLevelTables);
+			}
+			else 
+				tpProgression.getTabs().remove(tabSpellsKnown);
 		}
 		if (tableRowType != "pathfinder.data.Classes.Objects.LevelTable.MonkLevelTableRow")
 			tpProgression.getTabs().remove(tabMonkSpecials);
